fe9198fe72b41eb94a36380c3332dfeac34b00f6
David Leger <davidleger95@me.com>
2020-11-14 22:46:08 -0400

    fancy stuff

--- components/Podcast/index.tsx
+++ components/Podcast/index.tsx
@@ -1,20 +1,82 @@
 import React from 'react';
 import data from 'content/podcasts.json';
-import { Content, ContentWrapper, CoverImage, CoverImageWrapper, Section } from 'components/styled';
+import {
+  Artwork,
+  ArtworkWrapper,
+  Content,
+  ContentWrapper,
+  CoverImage,
+  CoverImageWrapper,
+  Section,
+  SectionHeader
+} from 'components/styled';
+import Wave from 'assets/wave.svg';
+import styled from 'styled-components';
 
-const AnyCoverImage = CoverImage as any;
+const StyledWave = styled(Wave)<{ color: string }>`
+  color: ${(p) => p.primaryColor};
+  width: 100%;
+  z-index: 1000;
+  transform: translateY(calc(-100% + 1px - 5vw));
+  margin-top: 5vw;
+  margin-bottom: -12vw;
+
+  @media (max-width: 600px) {
+    transform: translateY(calc(-100% + 1px - 5vw)) scaleY(2);
+    transform-origin: bottom;
+  }
+
+  @keyframes amplify {
+    0% {
+      transform: scaleY(0.75);
+    }
+    25% {
+      transform: scaleY(0.6);
+    }
+    50% {
+      transform: scaleY(0.8);
+    }
+    75% {
+      transform: scaleY(0.6);
+    }
+    100% {
+      transform: scaleY(1);
+    }
+  }
+
+  path {
+    transform-origin: bottom;
+    animation: amplify 11s ease-in-out infinite alternate;
+
+    &:nth-child(1) {
+      animation-delay: -2s;
+      animation-duration: 7s;
+    }
+    &:nth-child(2) {
+      animation-delay: -4s;
+      animation-duration: 9s;
+    }
+  }
+`;
+
 const Podcasts = () => {
   return (
     <>
-      {data.map(({ theme, coverImage, name, host, about, featuredEpisode }) => (
+      {data.map(({ theme, coverImage, artwork, name, host, about, featuredEpisode }) => (
         <Section {...theme} key={name}>
           <CoverImageWrapper>
-            <AnyCoverImage {...coverImage} layout="fill" />
+            <CoverImage {...coverImage} layout="fill" />
           </CoverImageWrapper>
           <ContentWrapper>
+            <StyledWave color={theme.primaryColor} />
             <Content>
-              <h2>{name}</h2>
-              <h3>{host}</h3>
+              <SectionHeader>
+                <ArtworkWrapper>
+                  <Artwork src={artwork} width={200} height={200} layout="responsive" />
+                </ArtworkWrapper>
+                <h2>{name}</h2>
+                <h3>{host}</h3>
+              </SectionHeader>
               <div dangerouslySetInnerHTML={{ __html: about }} />
               <iframe
                 src={`https://open.spotify.com/embed-podcast/episode/${featuredEpisode.id}`}
--- components/styled/index.ts
+++ components/styled/index.ts
@@ -13,18 +13,36 @@ export const ContentWrapper = styled.div`
   position: relative;
 `;
 export const Content = styled.div`
-  max-width: 1000px;
+  max-width: 640px;
   margin: auto;
   padding: 2rem;
 
   h2 {
-    font-size: 5rem;
+    font-size: clamp(3rem, 10vw, 5rem);
     margin: 0;
+    text-align: center;
+  }
+
+  h3 {
+    margin: 0;
+    opacity: 0.6;
   }
 
   p {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
       'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  p:first-of-type {
+    &::first-letter {
+      font-family: 'Bebas Neue', sans-serif;
+      font-size: 3.75rem;
+      display: block;
+      float: left;
+      margin-right: 0.15em;
+      line-height: 1;
+      margin-bottom: -0.25em;
+    }
   }
 `;
 
@@ -32,11 +50,22 @@ export const Section = styled.section<SectionProps>`
   ${ContentWrapper} {
     color: ${(p) => p.accentColor};
     background-color: ${(p) => p.primaryColor};
+
+    p {
+      color: ${(p) => p.textColor};
+    }
   }
 `;
 
+export const SectionHeader = styled.header`
+  text-align: center;
+  display: grid;
+  gap: 1rem;
+  margin-bottom: 4rem;
+`;
+
 export const CoverImageWrapper = styled.div`
-  height: clamp(10rem, 60vmin, 70vh);
+  height: clamp(10rem, calc(5rem + 60vmin), calc(5rem + 70vh));
   width: 100vw;
   overflow: hidden;
   position: sticky;
@@ -44,7 +73,21 @@ export const CoverImageWrapper = styled.div`
   z-index: 0;
 `;
 
-export const CoverImage = styled(Image)`
+export const CoverImage: typeof Image = styled(Image)`
   object-fit: cover;
   object-position: top;
+`;
+
+export const Artwork: typeof Image = styled(Image)`
+  display: block;
+  border-radius: 1rem;
+`;
+
+export const ArtworkWrapper = styled.div`
+  margin: auto;
+  width: clamp(8rem, 30vw, 12rem);
+  margin-top: calc(-150px - 5vw);
+  transform: rotateZ(7deg);
+  margin-bottom: 3rem;
+  filter: drop-shadow(0 0 1rem #0008);
 `;
